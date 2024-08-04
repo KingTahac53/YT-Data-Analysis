f5a4fd61532f4fed79b7bd99c1be727df43abff4
KingTahac53 <tahac59@gmail.com>
2024-08-04 18:24:04 +0530

    get channels colab code version

--- channels/channels_colab.py
+++ channels/channels_colab.py
@@ -6,13 +6,14 @@ from googleapiclient.discovery import build
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
+
 def main():
     auth.authenticate_user()
     credentials, project = google.auth.default(scopes=scopes)
-    
+
     api_service_name = "youtube"
     api_version = "v3"
-    
+
     youtube = build(api_service_name, api_version, credentials=credentials)
 
     request = youtube.channels().list(
@@ -21,6 +22,7 @@ def main():
     response = request.execute()
 
     print(json.dumps(response, indent=4))
+
 
 if __name__ == "__main__":
     main()
